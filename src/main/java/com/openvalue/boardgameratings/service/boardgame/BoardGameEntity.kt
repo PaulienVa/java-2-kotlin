38f54bb1c5efa41e913fef7b35e9f2bff34d4aa6
Paulien <>
2018-09-26 18:18:50 +0200

    [SERVICE] - migrate the spring class

---
+++ src/main/java/com/openvalue/boardgameratings/service/boardgame/BoardGameEntity.kt
@@ -0,0 +1,2 @@
+package com.openvalue.boardgameratings.service.boardgame
+
--- src/main/java/com/openvalue/boardgameratings/service/boardgame/Category.java
+++
@@ -1,8 +0,0 @@
-package com.openvalue.boardgameratings.service.boardgame;
-
-public enum Category {
-    EDUCATIONAL,
-    STRATEGY,
-    ADVENTURE,
-    ;
-}
