2d1ac2ddad487d807db55a29cc013971f84d7a11
van Alst, P <paulien@openvalue.nl>
2018-09-27 20:39:35 +0200

    [SERVICE] - removed unnecessary comments

--- src/main/java/com/openvalue/boardgameratings/api/Rating.java
+++ src/main/java/com/openvalue/boardgameratings/api/Rating.java
@@ -5,9 +5,4 @@ import lombok.Value;
 @Value
 public class Rating {
     private final Double averageRate;
-    private final Integer scale;
-    //todo maybe some more
-
-    public static class Rate {
-    }
 }
