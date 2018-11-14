6bf510b84e639c66dfe88a35508be69575fd24bc
Alst, P. van (Paulien) <paulien@openvalue.nl>
2018-11-14 16:00:02 +0100

    [SERVICE] - refactored the formatting unit and integration test
    (cherry picked from commit ff39ed48dd17725519ecf644c55196f65e29c9f8)

--- src/test/java/com/openvalue/boardgameratings/service/rating/BoardGameRatingServiceTest.java
+++ src/test/java/com/openvalue/boardgameratings/service/rating/BoardGameRatingServiceTest.java
@@ -2,11 +2,9 @@ package com.openvalue.boardgameratings.service.rating;
 
 import com.openvalue.boardgameratings.api.BoardGame;
 import com.openvalue.boardgameratings.api.Rating;
-import com.openvalue.boardgameratings.api.request.RatingRequest;
 import com.openvalue.boardgameratings.service.boardgame.BoardGameRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -14,11 +12,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static com.openvalue.boardgameratings.service.util.TestData.*;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
@@ -36,13 +32,19 @@ class BoardGameRatingServiceTest {
 
     @Test
     void when_game_is_rated_then_the_game_with_new_average_rate_is_returned() {
-        when(boardGameRepository.findByName(POPULAR_GAME)).thenReturn(Optional.of(
-                popularBoardGame()
-        ));
+        when(boardGameRepository.findByName(POPULAR_GAME))
+                .thenReturn(Optional.of(
+                    popularBoardGame()
+                ));
 
-        when(rateRepository.findByBoardGameName(POPULAR_GAME)).thenReturn(Arrays.asList(highRate(), lowRate()));
+        when(rateRepository.findByBoardGameName(POPULAR_GAME))
+                .thenReturn(Arrays.asList(
+                        highRate(),
+                        lowRate()
+                ));
 
-        final BoardGame newlyRatedGame = boardGameRatingService.ratingBoardGame(new RateBoardGame(POPULAR_GAME, lowRate().getRate()));
+        final BoardGame newlyRatedGame = boardGameRatingService.ratingBoardGame(
+                new RateBoardGame(POPULAR_GAME, lowRate().getRate()));
 
         assertEquals(new Rating(3.0d), newlyRatedGame.getRating());
     }
@@ -55,13 +57,18 @@ class BoardGameRatingServiceTest {
                 notPopularBoardGame()
         ));
 
-        doReturn(Arrays.asList(highRate())).when(rateRepository).findByBoardGameName(POPULAR_GAME);
-        doReturn(Arrays.asList(lowRate())).when(rateRepository).findByBoardGameName(NOT_POPULAR_GAME);
+        doReturn(Arrays.asList(highRate()))
+                .when(rateRepository).findByBoardGameName(POPULAR_GAME);
 
-        final List<BoardGame> ratedHigherThan3 = boardGameRatingService.withHigherRateThan(3.0d);
+        doReturn(Arrays.asList(lowRate()))
+                .when(rateRepository).findByBoardGameName(NOT_POPULAR_GAME);
+
+        final List<BoardGame> ratedHigherThan3 = boardGameRatingService
+                .withHigherRateThan(3.0d);
 
         assertEquals(1, ratedHigherThan3.size());
-        assertEquals(1, ratedHigherThan3.stream().filter(game -> game.getName().equals(POPULAR_GAME)).count());
+        assertEquals(1, ratedHigherThan3.stream()
+                .filter(game -> game.getName().equals(POPULAR_GAME)).count());
     }
 
 }
